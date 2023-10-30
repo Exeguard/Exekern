e24775589f8b6d3003107589c6b5f9d3d41cac4b
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-30 22:42:34 +0000

    Make the module more specific to Exekern

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-static int __init mod_init(void) {
-    printk(KERN_INFO "Hello, world!\n");
+static int __init exekern_init(void) {
+    printk(KERN_INFO "Loaded Exekern.\n");
+
     return 0;
 }
 
-static void __exit mod_exit(void) {
-    printk(KERN_INFO "Goodbye, world!\n");
+static void __exit exekern_exit(void) {
+    printk(KERN_INFO "Unloaded Exekern.\n");
 }
 
-module_init(mod_init);
-module_exit(mod_exit);
+module_init(exekern_init);
+module_exit(exekern_exit);
 
 MODULE_LICENSE("MIT");
 MODULE_AUTHOR("Ben McAvoy");
-MODULE_DESCRIPTION("A kernel module to handle execution policies.");
+MODULE_DESCRIPTION("A kernel module for managing execution of programs.");
